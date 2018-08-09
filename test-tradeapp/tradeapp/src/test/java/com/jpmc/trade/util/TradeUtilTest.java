6f522f65eaf2959437fca3dfc5af73721a548260
nitin1502 <srivastava.nitin@gmail.com>
2018-08-09 08:19:07 +0100

    add few more test cases

--- test-tradeapp/tradeapp/src/test/java/com/jpmc/trade/util/TradeUtilTest.java
+++ test-tradeapp/tradeapp/src/test/java/com/jpmc/trade/util/TradeUtilTest.java
@@ -11,9 +11,27 @@ import com.jpmc.trade.enums.Currency;
 public class TradeUtilTest {
 
 	@Test
-	public void getBusinessDateByCurrencyTest() {
+	public void getBusinessDateByCurrencyTestForSAR_Friday() {
 		LocalDate businessDate = TradeUtil.getBusinessDateByCurrency(Currency.SAR, LocalDate.of(2018, 8, 10));
 		assertEquals(LocalDate.of(2018, 8, 12), businessDate);
 	}
 
+	@Test
+	public void getBusinessDateByCurrencyTestForAED_Saturday() {
+		LocalDate businessDate = TradeUtil.getBusinessDateByCurrency(Currency.AED, LocalDate.of(2018, 8, 11));
+		assertEquals(LocalDate.of(2018, 8, 12), businessDate);
+	}
+	
+	@Test
+	public void getBusinessDateByCurrencyTestForSGP_Sunday() {
+		LocalDate businessDate = TradeUtil.getBusinessDateByCurrency(Currency.SGP, LocalDate.of(2018, 8, 12));
+		assertEquals(LocalDate.of(2018, 8, 13), businessDate);
+	}
+	
+	@Test
+	public void getBusinessDateByCurrencyTestForSGP_Monday() {
+		LocalDate businessDate = TradeUtil.getBusinessDateByCurrency(Currency.SGP, LocalDate.of(2018, 8, 13));
+		assertEquals(LocalDate.of(2018, 8, 13), businessDate);
+	}
+	
 }
